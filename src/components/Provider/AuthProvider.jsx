74a3c33d58352e270464f187af3a9bda020edf8e
gfaruk02 <gfaruk02@gmail.com>
2023-10-18 01:40:26 +0600

    create login and register funtionality with manage user

---
+++ src/components/Provider/AuthProvider.jsx
@@ -0,0 +1,65 @@
+import { createContext, useEffect, useState } from "react";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import PropTypes from 'prop-types';
+import app from "../firebase/Firebase.config";
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState();
+    const [loading, setLoading] = useState(true)
+
+    const registerUser = (email, password) =>{
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const signInUser = (email, password) =>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const signOut = () =>{
+        setLoading(true);
+        return signOut(auth)
+    }
+
+    const googleSignIn = () =>{
+        return signInWithPopup(auth, provider)
+    }
+
+    useEffect( ()=>{
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) =>{
+            console.log('Manage User', currentUser);
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return () =>{
+            unSubscribe();
+        }
+    },[])
+
+    const authInfo = {
+        user,
+        loading,
+        registerUser,
+        signInUser,
+        signOut,
+        googleSignIn
+    }
+
+    return (
+
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
+
+AuthProvider.propTypes = {
+    children: PropTypes.node
+}
