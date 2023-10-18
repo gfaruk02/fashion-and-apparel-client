eec7a36cf2dbf568593eb8af34cf815e1c62dae9
gfaruk02 <gfaruk02@gmail.com>
2023-10-18 10:31:20 +0600

    create banner route and add slider in banner also set it home page

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,38 @@
+
+
+const Banner = () => {
+    return (
+        <div className="carousel w-full h-[90vh]">
+  <div id="slide1" className="carousel-item relative w-full">
+    <img src={'https://i.ibb.co/Z24tkbd/home-banner1.jpg'} className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" className="btn btn-circle">❮</a> 
+      <a href="#slide2" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide2" className="carousel-item relative w-full">
+    <img src={'https://i.ibb.co/3zBbjCY/relaxation-banner.jpg'} className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" className="btn btn-circle">❮</a> 
+      <a href="#slide3" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide3" className="carousel-item relative w-full">
+    <img src={'https://i.ibb.co/1zncg9C/IMG-2979.jpg'} className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide2" className="btn btn-circle">❮</a> 
+      <a href="#slide4" className="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide4" className="carousel-item relative w-full">
+    <img src={'https://i.ibb.co/cygLzJW/almonte-celtfest-scaled.jpg'} className="w-full" />
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide3" className="btn btn-circle">❮</a> 
+      <a href="#slide1" className="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default Banner;
